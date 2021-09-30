a34d0d4b9eddb71454f3743019db93ed1040e6e4
jamwelch <jamwelch@cisco.com>
2021-09-30 16:53:33 -0400

    Update Get_Orgs_Intersight.ps1

--- Get_Orgs_Intersight.ps1
+++ Get_Orgs_Intersight.ps1
@@ -1,21 +1,23 @@
 Install-Module -Name Intersight.PowerShell
 
-# Modify KeyId and Path as needed for the desired account. 
-# https://github.com/CiscoDevNet/intersight-powershell/blob/master/GettingStarted.md
-#
+# Modify API Key and Path to your secret key as needed for the desired account.
+$APIKEY = "YOUREXTREMELYLONGAPIKEYGOESHERE"
+$SECRETKEYPATH = "C:\PATH\TO\YOUR\SecretKey.txt"
+
 $connect = @{
     BasePath = "https://intersight.com"
-    ApiKeyId = "6155e2297564612d33fca3b4/6155e2297564612d33fca3b8/6155e3b47564612d30e377ed"
-    ApiKeyFilePath = "F:\PowerShell\Intersight\SecretKey.txt" 
-    HttpSingingHeader =  @("(request-target)", "Host", "Date", "Digest")
+    ApiKeyId = $APIKEY
+    ApiKeyFilePath = $SECRETKEYPATH
+    HttpSignerHeader =  @("(request-target)", "Host", "Date", "Digest")
 }
-#Note - "HttpSingingHeader" is the correct syntax - the API has a typo (could be fixed at some point in the future and your scripts would need to be modified to correct the typo)
+
 Set-IntersightConfiguration @connect
 Get-IntersightConfiguration
 
 #Get the List of Organizations
-$OrgList = Get-IntersightOrganizationOrganization
-#Get the OrganizationOrganization by Name
+Get-IntersightOrganizationOrganization
+
+#Get the Organization by Name - Change Org Name as desired
 $OrgName = "Default"
 $OrgByName = Get-IntersightOrganizationOrganization -Name $OrgName
 
