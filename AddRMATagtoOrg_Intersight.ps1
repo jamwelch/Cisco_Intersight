2aa0aecac60b9eb1de0971100246a227dfae053e
jamwelch <jamwelch@cisco.com>
2021-10-01 09:14:38 -0400

    Update AddRMATagtoOrg_Intersight.ps1

--- AddRMATagtoOrg_Intersight.ps1
+++ AddRMATagtoOrg_Intersight.ps1
@@ -6,7 +6,7 @@ $connect = @{
     BasePath = "https://intersight.com"
     ApiKeyId = $APIKEY
     ApiKeyFilePath = $SECRETKEYPATH
-    HttpSingerHeader =  @("(request-target)", "Host", "Date", "Digest")
+    HttpSingingHeader =  @("(request-target)", "Host", "Date", "Digest")
     # HttpSignerHeader =  @("(request-target)", "Host", "Date", "Digest")
     # Bug filed for this typo and will be fixed in next release.  
     # "HttpsignerHeader" should be used once the fix is in.
