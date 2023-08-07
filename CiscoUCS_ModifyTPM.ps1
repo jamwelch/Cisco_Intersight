80d21c820e18c06d58ae60387e0aa58f87f146da
jamwelch <jamwelch@cisco.com>
2023-08-07 10:29:30 -0400

    Update CiscoUCS_ModifyTPM.ps1

--- CiscoUCS_ModifyTPM.ps1
+++ CiscoUCS_ModifyTPM.ps1
@@ -1,3 +1,3 @@
-#This script will prompt user from input deciding whether to modify TPM settings for UCSM servers or standalone servers.
+#This script will prompt user from input deciding whether to modify TPM settings for UCSM servers or standalone C-Series servers.
 #IMM servers are not supported by the script.
 
